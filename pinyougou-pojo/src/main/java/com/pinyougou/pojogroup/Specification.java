da0e29a493a5c6bb96468aefd318364bac5aa974
xymyf1314 <xymyf6@163.com>
2020-04-08 21:40:27 +0800

    新增规格完成

---
+++ pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Specification.java
@@ -0,0 +1,43 @@
+package com.pinyougou.pojogroup;
+
+import com.pinyougou.pojo.TbSpecification;
+import com.pinyougou.pojo.TbSpecificationOption;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 规格组合实体类
+ *
+ * @author Fan
+ * @version 1.0
+ * @date 2020/4/6 21:40
+ */
+public class Specification implements Serializable {
+    private TbSpecification specification;
+    private List<TbSpecificationOption> specificationOptionList;
+
+    public Specification() {
+    }
+
+    public Specification(TbSpecification specification, List<TbSpecificationOption> specificationOptionList) {
+        this.specification = specification;
+        this.specificationOptionList = specificationOptionList;
+    }
+
+    public TbSpecification getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(TbSpecification specification) {
+        this.specification = specification;
+    }
+
+    public List<TbSpecificationOption> getSpecificationOptionList() {
+        return specificationOptionList;
+    }
+
+    public void setSpecificationOptionList(List<TbSpecificationOption> specificationOptionList) {
+        this.specificationOptionList = specificationOptionList;
+    }
+}
